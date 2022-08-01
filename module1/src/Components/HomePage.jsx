32877dfa8ec5ad709941fac19664c746fb56f037
Daksh Taneja <32364309+daksh3196@users.noreply.github.com>
2022-08-02 02:04:07 +0530

    Update HomePage.jsx

--- module1/src/Components/HomePage.jsx
+++ module1/src/Components/HomePage.jsx
@@ -3,6 +3,9 @@ import React from "react";
 const HomePage = ({name}) =>{
     return(
         <>
+            <h1>
+                React-Redux Course Class II - Basic To Do Application
+            </h1>
             <h3>
                 Assignment Submitted by {name}.
             </h3>
